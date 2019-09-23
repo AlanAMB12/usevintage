4edbc4d3f794b49ee268cd7527fbfe2c24d3ae08
AlanAMB12 <54220807+AlanAMB12@users.noreply.github.com>
2019-09-23 00:29:57 -0500

    Add files via upload

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //conexion a la base
+    database:{
+        host: 'b85pcf8ghx5pka5ftdvw-mysql.services.clever-cloud.com',
+        user: 'ugyez7erthgsxxsh',
+        password: 'ye6LXGtYJZwmkdz1hL4W',
+        database: 'b85pcf8ghx5pka5ftdvw'
+    }
+
+}
